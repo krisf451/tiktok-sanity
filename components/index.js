e17f8fae675be257448a0e43453f2420805ee3a7
Kristian Fulkerson <kristianfulkerson@Kristians-MacBook-Pro.local>
2022-07-02 16:37:36 -0700

    feat: fetching our videos from sanity be and implemnted play/pause and mute/fullvolume functionality

--- components/index.js
+++ components/index.js
@@ -3,3 +3,5 @@ export { default as Sidebar } from "./Sidebar";
 export { default as SuggestedAccounts } from "./SuggestedAccounts";
 export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
+export { default as NoResults } from "./NoResults";
+export { default as VideoCard } from "./VideoCard";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
 import type { NextPage } from "next";
+import axios from "axios";
 
-const Home: NextPage = () => {
+import { VideoCard, NoResults } from "../components";
+import { Video } from "../types";
+
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos, "TEST");
   return (
-    <div>
-      <h1 className="text-green-500">Tiktik</h1>
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"No Videos"} />
+      )}
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
 };
 
 export default Home;
